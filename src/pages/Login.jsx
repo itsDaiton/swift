006f63b7f24f26c8d5e4c1348cbf2b79845d5982
David Poslušný <david.poslusny@email.cz>
2023-02-28 15:38:59 +0100

    basic app structure + routing setup

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className="bg-[url('./src/assets/background.jpg')] h-screen w-screen">
+      Login page.    
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className="bg-[url('./src/assets/background.jpg')] h-screen w-screen">
+      Register page.  
+    </div>
+  )
+}
+
+export default Register
