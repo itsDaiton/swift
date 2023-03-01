4741ba6f46dfd72746425f40816239372404a927
David Poslušný <david.poslusny@email.cz>
2023-03-01 18:17:59 +0100

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useNavigate } from 'react-router'
+import { auth } from '../../utils/firebase'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Navbar = () => {
+
+	const [user, loading] = useAuthState(auth)
+
+	let navigate = useNavigate()
+
+	const handleSignOut = () => {
+    auth.signOut().then(() => {
+      navigate('/login')
+    })
+  }
+
+	const goToLogin = () => {
+		navigate('/login')
+	}
+
+	const goToRegister = () => {
+		navigate('/register')
+	}
+
+  return (
+    <div className='flex justify-between bg-black text-white h-[70px]'>
+			<div className='flex justify-center items-center pl-6 space-x-4'>
+				<FontAwesomeIcon icon={faPaperPlane} className='text-[36px]'/>
+				<p className='text-[36px] font-poppins tracking-widest font-bold'>
+					SWIFT
+				</p>
+			</div>
+			{user ?
+			<div className='flex justify-center items-center pr-6 space-x-6'>
+				<p 
+					className='text-[18px] font-poppins cursor-pointer font-semibold'
+					onClick={handleSignOut}
+				>
+					Sign Out
+				</p>
+				<img
+          src={user.photoURL}
+          className='w-[40px] h-[40px] object-contain rounded-full'
+        />
+			</div>
+			: 
+			<div className='flex justify-center items-center pr-6 space-x-6'>
+				<p 
+					className='text-[18px] font-poppins cursor-pointer font-semibold'
+					onClick={goToRegister}
+				>
+					Register
+				</p>
+				<p 
+					className='text-[18px] font-poppins cursor-pointer font-semibold'
+					onClick={goToLogin}
+				>
+					Login
+				</p>
+			</div>		
+			}
+    </div>
+  )
+}
+
+export default Navbar
