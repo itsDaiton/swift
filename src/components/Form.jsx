00482d03510704acb47eeb4f3c136da186426ed7
David Poslušný <david.poslusny@email.cz>
2023-03-08 16:47:02 +0100

    login page responsivness

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -123,19 +123,37 @@ const Form = ({ type }) => {
     }
   }
 
-  if (loading) {
-    return <div className='bg-gradient h-screen text-white'>Loading...</div>
-  }
+  
+  const oauth = [
+    {
+      text: 'Continue with Google',
+      icon: faGoogle,
+      color: '#db4a39',
+      action: googleAuth
+    },
+    {
+      text: 'Continue with Facebook',
+      icon: faFacebook,
+      color: '#3b5998',
+      action: facebookAuth
+    },
+    {
+      text: 'Continue with GitHub',
+      icon: faGithub,
+      color: '#24292F',
+      action: gitHubAuth
+    },
+  ]
 
   return (
-    <div className='w-[500px] h-[900px] glassmorphism'>
-      <div className='flex justify-center items-center pt-10'>
-        <h4 className='text-white text-[32px] font-poppins'>
+    <div className='glassmorphism sm:py-10 py-5 px-1 mx-10 my-10'>
+      <div className='flex justify-center items-center'>
+        <h4 className='text-white lg:text-[32px] md:text-[26px] sm:text-[24px] text-[20px] font-poppins'>
           {type === 'login' ? 'Login' : 'Register'}
         </h4>
       </div>
-      <form className='mt-[20px]' onSubmit={handleSubmit}>
-        <div className='mx-10 mt-8'>
+      <form onSubmit={handleSubmit}>
+        <div className='mx-10 lg:mt-8 mt-0'>
           <span className={`text-[16px] font-poppins mx-2 ${error ? 'text-red-600' : 'text-white'}`}>Email</span>
           <div className={`relative flex items-center ${error ? 'text-red-600' : 'text-white'} pt-1`}>
             <FontAwesomeIcon icon={faUser} className='text-[24px] absolute ml-3 pointer-events-none z-10'/>
@@ -145,7 +163,8 @@ const Form = ({ type }) => {
               autoComplete='off'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className='text-[20px] w-full h-[55px] outline-none border-none glass-input font-poppins text-semibold 
+              className='lg:text-[20px] md:text-[18px] sm:text-[18px] text-[16px] w-full sm:h-[55px] h-[45px] outline-none border-none 
+              glass-input font-poppins text-semibold 
               pl-[48px] pr-3 placeholder-current'
             />
           </div>
@@ -160,19 +179,21 @@ const Form = ({ type }) => {
               autoComplete='off'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className='text-[20px] w-full h-[55px] outline-none border-none glass-input font-poppins text-semibold
-               pl-[48px] pr-3 placeholder-current'
+              className='lg:text-[20px] md:text-[18px] sm:text-[18px] text-[16px] w-full sm:h-[55px] h-[45px] outline-none border-none 
+              glass-input font-poppins text-semibold
+              pl-[48px] pr-3 placeholder-current'
             />
           </div>
         </div>
-        <div className='flex justify-center items-center flex-col relative pt-10'>
-          <div className={`flex justify-center items-center glass-error text-white mb-10 text-sm font-poppins w-fit py-2 mx-8 ${error || providerError ? 'visible' : 'invisible'}`}>
+        <div className='flex justify-center items-center flex-col relative pt-5'>
+          <div className={`flex justify-center items-center glass-error text-white sm:mb-10 mb-4 text-sm font-poppins w-fit py-2 mx-8 ${error || providerError ? 'visible' : 'invisible'}`}>
             <FontAwesomeIcon icon={faTriangleExclamation} className='text-[24px] pr-4 pl-4'/>
             <p className='text-[14px] pr-4'>{error || providerError}</p>
           </div>
           <button 
             type='submit'
-            className='button-gradient text-[24px] text-white w-3/4 h-[60px] rounded-full shadow-xl font-poppins'
+            className='button-gradient lg:text-[24px] md:text-[22px] sm:text-[20px] text-[18px] text-white sm:py-5 py-4 px-16 rounded-full 
+            shadow-xl font-poppins'
           >
             {type === 'login' ? 'Sign In' : 'Sign Up'}
           </button>
@@ -182,42 +203,24 @@ const Form = ({ type }) => {
           <span className='flex-shrink mx-4 text-white'>Or</span>
           <div className='flex-grow border-t-2 border-white mr-10'/>
         </div>
-        <div className='flex justify-center items-center py-2'>
-          <button 
-              type='button'
-              className='text-[24px] text-white bg-[#db4a39] w-3/4 h-[60px] rounded-full shadow-xl font-poppins text-center
-              inline-flex items-center px-5 py-2.5'
-              onClick={googleAuth}
-            >
-              <FontAwesomeIcon icon={faGoogle} className='pr-5'/>
-              Continue with Google
-          </button>
-        </div>
-        <div className='flex justify-center items-center py-2'>
-          <button 
-              type='button'
-              className='text-[24px] text-white bg-[#3b5998] w-3/4 h-[60px] rounded-full shadow-xl font-poppins text-center
-              inline-flex items-center px-5 py-2.5'
-              onClick={facebookAuth}
-            >
-              <FontAwesomeIcon icon={faFacebook} className='pr-5'/>
-              Continue with Facebook
-          </button>
-        </div>
-        <div className='flex justify-center items-center py-2'>
-          <button 
-              type='button'
-              className='text-[24px] text-white bg-[#24292F] w-3/4 h-[60px] rounded-full shadow-xl font-poppins text-center
-              inline-flex items-center px-5 py-2.5'
-              onClick={gitHubAuth}
-            >
-              <FontAwesomeIcon icon={faGithub} className='pr-5'/>
-              Continue with GitHub
-          </button>
+        <div>
+          {oauth.map((provider, index) => (
+          <div className='flex justify-center items-center py-2 px-10' key={index}>
+            <button 
+                type='button'
+                className={`text-[24px] text-white sm:w-[100%] w-fit px-10 py-3 rounded-full shadow-xl font-poppins
+                text-center inline-flex items-center bg-[${provider.color}]`}
+                onClick={provider.action}
+              >
+                <FontAwesomeIcon icon={provider.icon} className='sm:pr-5 pr-0'/>
+                <span className='sm:flex hidden'>{provider.text}</span>
+            </button>
+          </div>          
+          ))}
         </div>
       </form>
       <div className='flex justify-center items-center pt-4'>
-        <p className='text-white font-poppins text-[18px] underline cursor-pointer' onClick={handleNavigate}>
+        <p className='text-white font-poppins lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px] underline cursor-pointer text-center px-10' onClick={handleNavigate}>
           {type === 'login' ? "Don't have an account yet? Sign Up" : "Already have an account? Sign In"}
         </p>
       </div>
